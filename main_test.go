9e6004d9b7f36b3c25f2f75a72f4e7319a418188
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-10-10 14:46:28 +0100

    adds test cases

--- main_test.go
+++ main_test.go
@@ -14,7 +14,53 @@ func TestOneDep(t *testing.T) {
 	}
 	want := "root"
 	if got[0] != want {
-		t.Errorf("%s => %q, want %q", cgfile, got, want)
+		t.Errorf("%s => %q, want %q", cgfile, got[0], want)
+	}
+	want = "j2"
+	if got[1] != want {
+		t.Errorf("%s => %q, want %q", cgfile, got[1], want)
+	}
+}
+
+func TestTwoDep(t *testing.T) {
+	cgfile := "two-dep.yaml"
+	got, err := loadNRun(cgfile)
+	if err != nil {
+		t.Errorf("Can't load call graph %s: %v", cgfile, err)
+		t.FailNow()
+	}
+	want := "root"
+	if got[0] != want {
+		t.Errorf("%s => %q, want %q", cgfile, got[0], want)
+	}
+	want0 := "j2"
+	want1 := "j3"
+	if !((got[1] == want0 && got[2] == want1) ||
+		(got[1] == want1 && got[2] == want0)) {
+		t.Errorf("%s => %q, want %q or %q", cgfile, got[1], want0, want1)
+	}
+}
+
+func TestDiamond(t *testing.T) {
+	cgfile := "diamond.yaml"
+	got, err := loadNRun(cgfile)
+	if err != nil {
+		t.Errorf("Can't load call graph %s: %v", cgfile, err)
+		t.FailNow()
+	}
+	want := "root"
+	if got[0] != want {
+		t.Errorf("%s => %q, want %q", cgfile, got[0], want)
+	}
+	want0 := "j2"
+	want1 := "j3"
+	if !((got[1] == want0 && got[2] == want1) ||
+		(got[1] == want1 && got[2] == want0)) {
+		t.Errorf("%s => %q, want %q or %q", cgfile, got[1], want0, want1)
+	}
+	want = "j4"
+	if got[3] != want {
+		t.Errorf("%s => %q, want %q", cgfile, got[3], want)
 	}
 }
 
