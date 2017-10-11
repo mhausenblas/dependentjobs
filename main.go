c91e2fbef5ce92413b73dfdc4adb60f96cb0e37f
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-10-11 15:38:35 +0100

    preps for usage

--- main.go
+++ main.go
@@ -3,24 +3,54 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
 )
 
 func main() {
+	cgfile := "./examples/one-dep.yaml"
+	if len(os.Args) > 1 {
+		cgfile = os.Args[1]
+	}
+	fromfile(cgfile)
+
 	// store the call graph:
 	// err := dj.Store("./examples/dump.yaml")
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
+}
 
-	// read call graph from file:
-	// dj := New()
-	// err := dj.FromFile("./examples/one-dep.yaml")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("%+v\n", dj)
+func fromfile(cgfile string) {
+	fmt.Printf("Creating call graph:\n")
+	dj := New()
+	err := dj.FromFile(cgfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%#v\n", dj)
+	fmt.Println("Running jobs in call graph:")
+	dj.Run()
+	dj.Complete()
+	fmt.Printf("Call sequence: %v\n", dj.CallSeq())
+}
 
+func manualcg() DependentJobs {
+	dj := New()
+	dj.Add("root", "job 1", 0)
+	dj.Add("j2", "job 2", 1)
+	dj.Add("j3", "job 3", 1)
+	dj.Add("j4", "job 4", 2)
+	dj.Add("j5", "job 5", 2)
+	dj.AddDependents("j4", "j5")
+	dj.AddDependents("j2", "j4")
+	dj.AddDependents("j3", "j4")
+	dj.AddDependents("root", "j2", "j3", "j5")
+	fmt.Printf("%#v\n", dj)
+	return dj
+}
+
+func djcron() {
 	// run the call graph and print the call sequence:
 	jticks = make(map[string]int)
 	cycle := 0
@@ -48,19 +78,4 @@ func main() {
 		time.Sleep(1 * time.Second)
 		cycle++
 	}
-}
-
-func manualcg() DependentJobs {
-	dj := New()
-	dj.Add("root", "job 1", 0)
-	dj.Add("j2", "job 2", 1)
-	dj.Add("j3", "job 3", 1)
-	dj.Add("j4", "job 4", 2)
-	dj.Add("j5", "job 5", 2)
-	dj.AddDependents("j4", "j5")
-	dj.AddDependents("j2", "j4")
-	dj.AddDependents("j3", "j4")
-	dj.AddDependents("root", "j2", "j3", "j5")
-	fmt.Printf("%#v\n", dj)
-	return dj
 }
